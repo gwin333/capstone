9db1358af7912bc734555d92afa7fd9e177d8075
gwin333 <yang.290@gmail.com>
2019-04-02 12:03:25 -0500

    Updates

---
+++ precache-manifest.8d3451946d4edb1591dc8a71cef060e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30994b724e24a422cee2",
+    "url": "/my-app/static/css/main.0a23a176.chunk.css"
+  },
+  {
+    "revision": "30994b724e24a422cee2",
+    "url": "/my-app/static/js/main.7e15414f.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "0ed03e212d198994a633",
+    "url": "/my-app/static/css/2.d9705158.chunk.css"
+  },
+  {
+    "revision": "0ed03e212d198994a633",
+    "url": "/my-app/static/js/2.0a9f558c.chunk.js"
+  },
+  {
+    "revision": "ca89e8f7a4bc2dc95f00a85793faf62f",
+    "url": "/my-app/index.html"
+  }
+];
